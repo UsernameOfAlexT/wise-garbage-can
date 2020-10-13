1655ee8689215e6e6facc0f03a6c34f380d484bb
UsernameOfAlexT <jjcctamale@gmail.com>
2020-10-12 20:09:42 -0400

    update tithetaker responses and add extra utils

--- utils.js
+++ utils.js
@@ -11,3 +11,25 @@ exports.pickSafely = function (targetIndex, sourceList) {
 exports.randomInt = function (max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
+
+/**
+ * Returns true/false randomly with the percent chance of
+ * true being the given value
+ * 
+ * @param {number} chance 
+ */
+exports.withChance = function (chance) {
+  // shift up by one as randomInt actually gives integers 0-99 inclusive
+  return (exports.randomInt(100) + 1) <= chance;
+}
+
+/**
+ * Pick a random entry from the provided array
+ * 
+ * @param {Array} choices 
+ */
+exports.pickRandomly = function (choices) {
+  let choseni = exports.randomInt(choices.length);
+  let choiceItem = exports.pickSafely(choseni, choices);
+  return choiceItem;
+}
